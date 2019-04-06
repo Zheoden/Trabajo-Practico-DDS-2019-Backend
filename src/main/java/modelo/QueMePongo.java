31804fd5dd2fccdbba56a1652be692ddd202ca52
AxelFulop <axelfulop@hotmail.com>
2019-04-06 17:17:46 -0300

    usuario +test

---
+++ src/main/java/modelo/QueMePongo.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class QueMePongo {
+	public static void main(String[] args) {
+		
+	}
+
+	public String sugerirAtuendoPara(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
