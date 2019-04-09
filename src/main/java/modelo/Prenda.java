223d40b1c280b610e046c3a388d09f41a644eede
Marcos Ortiz <ortizmarcosn@gmail.com>
2019-04-09 19:46:58 -0300

    Subo requerimiento de las prendas correspondientes teniendo en cuenta dominio

--- src/main/java/modelo/Prenda.java
+++ src/main/java/modelo/Prenda.java
@@ -1,3 +1,11 @@
 package modelo;
+import modelo.ClasesUtilsPrendas;
+
 public class Prenda {
+	
+	Tipo.Tipos tipo; //remera, short, pantalón, etc.
+	ClasesUtilsPrendas.Tela tela; //algodón, seda, cuero, etc.
+	ClasesUtilsPrendas.Color colorPrimario; //color primario de la prenda. No puede ser NULL.
+	ClasesUtilsPrendas.Color colorSecundario; //color secundario de la prenda. Puede ser NULL. 
+	
 }
---
+++ src/test/java/TP1/PrendasTest.java
@@ -0,0 +1,14 @@
+package TP1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PrendasTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
