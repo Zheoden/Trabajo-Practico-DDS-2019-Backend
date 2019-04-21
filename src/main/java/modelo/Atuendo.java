c798e71e07d9b03ac028cb168a37c54256aa2df8
federicosolomonovici <fedesolo93@gmail.com>
2019-04-21 18:09:36 -0300

    estructura de atuendo

--- src/main/java/modelo/Atuendo.java
+++ src/main/java/modelo/Atuendo.java
@@ -1,5 +1,20 @@
 package modelo;
 
-public class Atuendo {
+import java.util.ArrayList;
 
+public class Atuendo {
+	
+	ArrayList<Prenda> prendas = new ArrayList<Prenda>();
+	
+	public Atuendo (ArrayList<Prenda> _prendas) {
+		prendas = _prendas;
+	}	
+	
+	public ArrayList<Prenda> getPrendas() {
+		return prendas;
+	}
+
+	// no valido el atuendo. Eso lo vamos a hacer en el algoritmo para generar atuendos
+	// tampoco hago genero setter ya que no veo sentido generar un atuendo sin prendas. A lo sumo luego vamos a tener funciones para agregar o sacar de la lista
+	
 }
