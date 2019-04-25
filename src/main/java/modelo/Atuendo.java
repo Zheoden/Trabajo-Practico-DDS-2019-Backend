fa957f17ff25c6f0a24643b37724179783a3e9a4
federicosolomonovici <fedesolo93@gmail.com>
2019-04-25 19:28:37 -0300

    cambios atuendo

--- src/main/java/modelo/Atuendo.java
+++ src/main/java/modelo/Atuendo.java
@@ -6,15 +6,12 @@ public class Atuendo {
 	
 	ArrayList<Prenda> prendas = new ArrayList<Prenda>();
 	
-	public Atuendo (ArrayList<Prenda> _prendas) {
-		prendas = _prendas;
+	public Atuendo (ArrayList<Prenda> ListaDePrendas) {
+		this.prendas = ListaDePrendas;
 	}	
 	
 	public ArrayList<Prenda> getPrendas() {
 		return prendas;
 	}
 
-	// no valido el atuendo. Eso lo vamos a hacer en el algoritmo para generar atuendos
-	// tampoco hago genero setter ya que no veo sentido generar un atuendo sin prendas. A lo sumo luego vamos a tener funciones para agregar o sacar de la lista
-	
 }
