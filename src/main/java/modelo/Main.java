09681d6de1fcdd09dfc2da60bef72ef9d4372c38
unknown <schifferjulian@gmail.com>
2019-05-28 22:06:49 -0300

    change default port for 7071, because i can

--- src/main/java/modelo/Main.java
+++ src/main/java/modelo/Main.java
@@ -1,10 +1,12 @@
 package modelo;
 
+import static spark.Spark.*;
 import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
+		port(7071);
 		new Usuario(new ArrayList<Guardarropas>());
 	}
 }
