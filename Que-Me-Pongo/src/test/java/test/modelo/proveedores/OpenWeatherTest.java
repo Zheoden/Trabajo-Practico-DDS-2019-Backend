21c02b61c25a271b04ae94c067f1319359e59e8c
unknown <schifferjulian@gmail.com>
2019-07-02 00:05:09 -0300

    Add template tests for Open Weatjer

--- Que-Me-Pongo/src/test/java/test/modelo/proveedores/OpenWeatherTest.java
+++ Que-Me-Pongo/src/test/java/test/modelo/proveedores/OpenWeatherTest.java
@@ -6,15 +6,31 @@ import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 
+@DisplayName("Tests para el proveedor OpenWeather")
 public class OpenWeatherTest {
+	
+	OpenWeather proveedor = new OpenWeather();
 
 	@Test
+	@DisplayName("Test para obtener la temperatura actual")
+	public void obtenerPronosticoActual() {
+		
+	}
+	
+	@Test
+	@DisplayName("Test para validar los pronosticos de toda la semana")
+	public void obtenerPronosticoExtendido() {
+
+	}
+	
+	@Test
 	@DisplayName("Test para obtener la temperatura del dia X")
-	public void getClientsFromJSON() throws ParseException {
-		OpenWeather proveedor = new OpenWeather();
+	public void obtenerTemperaturATalDia() {
+		
 
 		String localDate = "2019-07-07 00:00:00";
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
@@ -22,5 +38,11 @@ public class OpenWeatherTest {
 
 		System.out.println(proveedor.obtenerTemperaturATalDia(fechaDelClima));
 
+	}
+	
+	@Test
+	@DisplayName("Test para validar si el proveedor esta activo")
+	public void estaActivo() {
+		Assert.assertTrue(proveedor.estaActivo());
 	}
 }
