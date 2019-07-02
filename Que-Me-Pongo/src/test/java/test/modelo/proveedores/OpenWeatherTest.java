de091d65cd99cf376becfaca38abe05c23f74a41
unknown <schifferjulian@gmail.com>
2019-07-02 00:18:50 -0300

    Fixeo los tests de los utils

--- Que-Me-Pongo/src/test/java/test/modelo/proveedores/OpenWeatherTest.java
+++ Que-Me-Pongo/src/test/java/test/modelo/proveedores/OpenWeatherTest.java
@@ -1,8 +1,9 @@
 package test.modelo.proveedores;
 
+import modelo.proveedores.openweather.ExtendedOpenWeatherDTO;
 import modelo.proveedores.openweather.OpenWeather;
+import modelo.proveedores.openweather.OpenWeatherDTO;
 
-import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -12,34 +13,33 @@ import org.junit.jupiter.api.DisplayName;
 
 @DisplayName("Tests para el proveedor OpenWeather")
 public class OpenWeatherTest {
-	
+
 	OpenWeather proveedor = new OpenWeather();
 
 	@Test
 	@DisplayName("Test para obtener la temperatura actual")
 	public void obtenerPronosticoActual() {
-		
+		OpenWeatherDTO pronostico = proveedor.obtenerPronosticoActual();
+		Assert.assertTrue(pronostico != null);
 	}
-	
+
 	@Test
 	@DisplayName("Test para validar los pronosticos de toda la semana")
 	public void obtenerPronosticoExtendido() {
-
+		ExtendedOpenWeatherDTO pronostico = proveedor.obtenerPronosticoExtendido();
+		Assert.assertFalse(pronostico.getList().isEmpty());
 	}
-	
+
 	@Test
 	@DisplayName("Test para obtener la temperatura del dia X")
 	public void obtenerTemperaturATalDia() {
-		
-
 		String localDate = "2019-07-07 00:00:00";
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 		LocalDate fechaDelClima = LocalDate.parse(localDate, formatter);
 
-		System.out.println(proveedor.obtenerTemperaturATalDia(fechaDelClima));
-
+		Assert.assertTrue(proveedor.obtenerTemperaturATalDia(fechaDelClima) != null);
 	}
-	
+
 	@Test
 	@DisplayName("Test para validar si el proveedor esta activo")
 	public void estaActivo() {
--- Que-Me-Pongo/src/test/java/test/modelo/utils/JsonParserTest.java
+++ Que-Me-Pongo/src/test/java/test/modelo/utils/JsonParserTest.java
@@ -1,14 +1,14 @@
 package test.modelo.utils;
 
-import modelo.utils.JsonParser;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
+@DisplayName("Tests para el Parser de Jackson")
 public class JsonParserTest {
 
-	private JsonParser parser = new JsonParser();
-
 	@Test
-	public void getClientsFromJSON() {
+	@DisplayName("Tests para un Objecto a partir de un JSON")
+	public void getObjectFromJSON() {
 
 	}
 }
