958f404651231e70980b81aa54f60da7f79dd3c4
axelfulop <axelfulop@hotmail.com>
2019-10-10 09:43:29 -0300

    arreglo una query del atuendo

--- Que-Me-Pongo/src/main/java/repository/GuardarropaRepository.java
+++ Que-Me-Pongo/src/main/java/repository/GuardarropaRepository.java
@@ -15,7 +15,7 @@ public class GuardarropaRepository implements Repository<Guardarropas> {
 	
 	@SuppressWarnings("unchecked")
 	public List<Guardarropas> findByUser(String username){
-		String query = "SELECT g FROM Guardarropas g JOIN GuardarropaPorUsuario gpu ON (gpu.guardarropas_id = g.id) JOIN Usuario u ON(gpu.usuario_id = u.id) WHERE u.username = :username";
+		String query = "SELECT g FROM Guardarropas g JOIN GuardarropaPorUsuario gpu ON (gpu.guardarropas_id = g.id) JOIN Usuario u ON (gpu.usuario_id = u.id) WHERE u.username = :username";
 	    List<Guardarropas> guardarropas = entityManager().createQuery(query).getResultList();
 	    return guardarropas;
 	}
