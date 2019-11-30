e2df0de7a66ac0944f213b153125f2056e2c7cbf
Zheoden <schifferjulian@gmail.com>
2019-11-29 22:44:34 -0300

    Try the last hope for pom.xml

--- Que-Me-Pongo/src/main/java/modelo/interfaces/Repository.java
+++ Que-Me-Pongo/src/main/java/modelo/interfaces/Repository.java
@@ -6,28 +6,37 @@ import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
 public interface Repository<T> extends WithGlobalEntityManager, TransactionalOps {
 	default void persist(T entity) {
-		withTransaction(() -> entityManager().persist(entity));
+		withTransaction(() -> getEntityManager().persist(entity));
 	}
 
 	@SuppressWarnings("unchecked")
 	default void persistAll(T... entities) {
 		withTransaction(() -> {
-			Arrays.stream(entities).forEach(entityManager()::persist);
+			Arrays.stream(entities).forEach(getEntityManager()::persist);
 		});
 	}
 
 	default void delete(T entity) {
-		withTransaction(() -> entityManager().remove(entity));
+		withTransaction(() -> getEntityManager().remove(entity));
 	}
 
 	default void update(T entity) {
 		beginTransaction();
-		entityManager().flush();
+		getEntityManager().flush();
 		commitTransaction();
 	}
 
 	List<T> all();
 
+	public static EntityManager getEntityManager() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("QueMePongoDB");
+	    return emf.createEntityManager();
+	}
 }
