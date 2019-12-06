7b9b753a7371940e7cfa46614f5632a2b9935079
Zheoden <schifferjulian@gmail.com>
2019-12-06 18:30:32 -0300

    Agrego la S

--- Que-Me-Pongo/src/main/java/modelo/clases/Guardarropas.java
+++ Que-Me-Pongo/src/main/java/modelo/clases/Guardarropas.java
@@ -15,8 +15,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -34,7 +32,7 @@ public class Guardarropas {
 	long id;
 	
 	private String nombre;
-	@OneToMany(mappedBy = "guardarropa", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
+	@OneToMany(mappedBy = "guardarropas", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
 	private List<Prenda> prendas = new ArrayList<>();
 	@Transient
 	@JsonIgnore
